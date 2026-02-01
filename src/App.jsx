a05fd7da7cfad84ccf97a9315d873ebf9b4b0d44
OmarCGH <ocooper2024@fau.edu>
2026-02-01 13:27:30 -0500

    Adding tables next

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css';
-
+import './App.css'; Calendar;
+ 
 const App = () => {
 
   return (
     <div className="App">
-
-      
+    <h1>Ramo Repooc's Schedule</h1>
+      <h2>Hello! This is the schedule for Ramo Repooc's week.</h2>
+      <Calendar />
     </div>
   )
 }
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,23 @@
+const Calendar = () => {
+  return (
+    <div className="Calendar"> 
+        <table>
+            <thead>
+                <tr>
+                  
+                    <th>Sunday</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    
+    )
+}
+
+export default Calendar;
