dacb936533797401e749e97a3d208f3f8cfa9645
OmarCGH <ocooper2024@fau.edu>
2026-02-01 23:05:38 -0500

    Finished all coloring and formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'; 
 import Calendar from './components/calendar';
 
+
 const App = () => {
 
   return (
     <div className="App">
     <h1>Ramo Repooc's Schedule</h1>
-      <h2>Hello! This is the schedule for Ramo Repooc's week.</h2>
+      <h2>Hello! This is a general schedule for Ramo Repooc's week for the Spring Semester of 2026!</h2>
       <Calendar />
     </div>
   )
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,10 +1,12 @@
+import Event from './event'
+
 const Calendar = () => {
   return (
     <div className="Calendar"> 
-        <table>
+        <table> 
             <thead>
                 <tr>
-                    <th>    </th>
+                    <th>Day /<br></br>Time</th>
                     <th>Sunday</th>
                     <th>Monday</th>
                     <th>Tuesday</th>
@@ -12,8 +14,135 @@ const Calendar = () => {
                     <th>Thursday</th>
                     <th>Friday</th>
                     <th>Saturday</th>
-                </tr>
+                </tr> 
             </thead>
+            <tbody>
+                <tr>
+                    <td className="time">8AM</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr> 
+
+                <tr>
+                    <td className="time">9AM</td>
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <td></td>
+                </tr> 
+
+                <tr>
+                    <td className="time">10AM</td>
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td className="time">11AM</td>
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td className="time">12PM</td>
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <td></td>
+                </tr> 
+
+                <tr>
+                    <td className="time">1PM</td>
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Attend Lectures 📚' location="Boca Raton" color ='blue'/>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td className="time">2PM</td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                </tr>
+
+                <tr>
+                    <td className="time">3PM</td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                </tr>
+
+                <tr>
+                    <td className="time">4PM</td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                </tr>
+
+                <tr>
+                    <td className="time">5PM</td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Work at Internship 💵' location='West Palm Beach' color ='red' />
+                    <td></td>
+                    <Event event='Play 2XKO 🎮' location="Sunrise" color ='green'/>
+                </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </tbody>    
         </table>
     </div>
     
